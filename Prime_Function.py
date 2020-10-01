f3a1bfcf2566138f4eb77015bed9c20575cee9c1
Pinak Datta <pinakdatta2002@gmail.com>
2020-10-01 15:36:48 +0530

    New and better method to check for prime

--- Prime_Function.py
+++ Prime_Function.py
@@ -1,17 +1,15 @@
-def is_prime(p):
-    if p > 1:
-        n = p // 2
-        for i in range(2, n + 1):
-            if p % i == 0:
-                return False
-        return True
-    else:
+import math 
+def is_prime(n): 
+    if n <= 1: 
         return False
-
-number = 1011013
-
- # test data:  number = 1011013, number = 10110133, number = 101101331
-if is_prime(number):
-    print str(number) + " is a prime number"
+  
+    max_div = math.floor(math.sqrt(n)) 
+    for i in range(2, 1 + max_div): 
+        if n % i == 0: 
+            return False
+    return True
+n=int(input())
+if is_prime(n) :
+    print('Yes it is Prime')
 else:
-    print str(number) + " is not a prime number"
+    print('No it is not Prime')
